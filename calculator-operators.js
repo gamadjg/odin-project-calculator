6ca4225bb50988df2688c45a488dc95fb6342328
gamadjg <gamadavid36@gmail.com>
2023-03-15 07:41:19 -0700

    Deleted backend and operators js files

--- calculator-operators.js
+++
@@ -1,9 +0,0 @@
-const numBtns = document.querySelectorAll(".numBtn");
-let i = 9;
-numBtns.forEach((btn) => {
-	const newNumBtn = document.createElement("button");
-	newNumBtn.classList.add("btnStyles");
-	newNumBtn.textContent = i;
-	btn.appendChild(newNumBtn);
-	i--;
-});
